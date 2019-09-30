36b41132df03e33b296161c94183fe9b6a97e498
Mr-Frog <niklas.frerichs@gmail.com>
2019-09-30 03:47:00 +0200

    restructered database + better role detection + better stats

--- fantasy_utils.py
+++ fantasy_utils.py
@@ -6,15 +6,14 @@ import pandas as pd
 import os
 from PIL import Image, ImageDraw, ImageFont
 import time
-from io import StringIO
+import numpy as np
 from joblib import dump, load
 stratz_URL = 'http://api.stratz.com/api/v1/'
 opendota_URL = 'http://api.opendota.com/api/'
 
-def get_herostats():
+def get_herostats(heroStats):
     '''get heroStats.json from opendota for hero names/pics'''
     
-    heroStats = 'heroStats.json'
     if not os.path.isfile(heroStats):
         h_s = requests.get(opendota_URL+'heroStats').json()
         with open(heroStats, "w") as hs:
@@ -34,29 +33,32 @@ def get_games(league_id, from_date, take):
         game_date = datetime.date.fromtimestamp(y['startDateTime'])
         if from_date == 0:
             game_list.append(y['id'])
-        elif game_date == from_date:
+        elif game_date >= from_date:
             game_list.append(y['id'])
+    print("Found %s games."%len(game_list))
     return game_list
 
 
 
-def calc_fscore(kills, deaths, lh, den, gpm, tower_kill, rosh_kill, tf_participation,
-                obs_placed, camp_stacked, rune_taken, first_blood, stun_time):
+def calc_fscore(player, game):
     '''calculate fantasy score for a given player'''
+    #Calculate Teamfight participation
+    team_score = (game['dire_score'], game['radiant_score'])[player['isRadiant']]
+    tf_participation = round((player['kills'] + player['assists']) / team_score, 2)
     
     return round((
-    0.3 * kills
-    + 3 - 0.3 * deaths
-    + 0.003 * lh
-    + 0.003 * den
-    + 0.002 * gpm
-    + tower_kill
-    + rosh_kill
-    + 0.5 * obs_placed
-    + 0.5 * camp_stacked
-    + 0.25 * rune_taken
-    + 4 * first_blood
-    + 0.05 * stun_time
+    0.3 * player['kills']
+    + 3 - 0.3 * player['deaths']
+    + 0.003 * player['last_hits']
+    + 0.003 * player['denies']
+    + 0.002 * player['gold_per_min']
+    + player['tower_kills']
+    + player['roshan_kills']
+    + 0.5 * player['obs_placed']
+    + 0.5 * player['camps_stacked']
+    + 0.25 * player['rune_pickups']
+    + 4 * player['firstblood_claimed']
+    + 0.05 * player['stuns']
     + 3 * tf_participation
     ), 1)
     
@@ -92,121 +94,180 @@ def make_raw(game_list, RAW):
         raw_file.write(']')
     return 
     
+def prune_data(player, game, hero_data, obs_frame, sen_frame):
+
+    #Calculate Fantasy Score (Valve)
+    fscore = calc_fscore(player, game)
     
+    #Calculate Impact (Linail)
+    impact = calc_impact(player, game)
+    
+    #Convert hero_id to Hero Name + Hero Pic
+    hero_id = player['hero_id']
+    index = hero_data.index[hero_data['id'] == hero_id].tolist()
+    hero = hero_data.iat[index[0], 2]
+    hero_pic = hero_data.iat[index[0], 6]
+    
+    #Calculate Damage Taken by Heroes
+    damage_taken = 0
+    for instance, value in player['damage_taken'].items():
+        if 'hero' in instance:
+            damage_taken += value
+            
+    #Get Name
+    name = player['personaname']
+    if player['name'] != None:
+       name = ''.join(i for i in player['name'] if ord(i)<128)
+    
+    #Observer and Sentry Durations
+    obs_durations = []
+    sentry_durations = []
+    if len(player['obs_log']) > 0:
+        for obs in player['obs_log']:
+            time, handle = obs['time'], obs['ehandle']
+            try:
+                time_des = obs_frame[obs_frame['Ward ID'] == obs['ehandle']].iat[0, 0]
+            except:
+                time_des = game['duration']
+            time_alive = time_des - time
+            if time_alive > 360:
+                time_alive = 360
+            obs_durations.append(time_alive)
+    if len(player['sen_log']) > 0:
+        for sen in player['sen_log']:
+            time, handle = sen['time'], sen['ehandle']
+            try:
+                time_des = sen_frame[sen_frame['Ward ID'] == sen['ehandle']].iat[0, 0]
+            except:
+                time_des = game['duration']
+            time_alive = time_des - time
+            if time_alive > 360:
+                time_alive = 360
+            sentry_durations.append(time_alive)   
+    sum_obs = 0
+    sum_sen = 0
+    if len(obs_durations) > 0:
+        sum_obs = np.sum(obs_durations)
+    if len(sentry_durations) > 0:
+        sum_sen = np.sum(sentry_durations)
+    role = 0
+    impact = 0
+    start_date = datetime.date.fromtimestamp(game['start_time']).strftime("%Y-%m-%d")
+    
+    # Populate Player Frame
+    player_frame = pd.DataFrame(
+                [(game['match_id'], player['account_id'], name, hero, hero_pic, game['duration'],
+                player['kills'], player['deaths'], player['assists'], player['last_hits'],
+                player['denies'], player['benchmarks']['lhten']['raw'], player['dn_t'][10], 
+                player['gold_per_min'], player['xp_per_min'], player['hero_damage'], player['hero_healing'],
+                damage_taken, player['tower_damage'], player['tower_kills'], player['camps_stacked'],
+                player['obs_placed'], sum_obs, player['observer_kills'], player['sen_placed'],
+                sum_sen, player['sentry_kills'], player['teamfight_participation'], 
+                player['stuns'], player['firstblood_claimed'], player['rune_pickups'], player['roshan_kills'],
+                player['lane_role'], role, fscore, impact)],
+        columns = ['Match ID', 'Account ID', 'Player', 'Hero', 'Hero Picture',
+                'Game Length', 'Kills', 'Deaths', 'Assists', 'Last Hits', 'Denies', 'LH@10',
+                'Den@10', 'GPM', 'XPM', 'Hero Damage', 'Hero Healing', 'Damage Taken',
+                'Tower Damage', 'Tower Kills', 'Camps Stacked', 'Obs Placed', 'Total Obs Duration',
+                'Obs Killed', 'Sentry Placed', 'Total Sentry Duration', 'Sentry Killed', 
+                'Teamfight Participation', 'Stun Duration', 'First Blood', 'Rune Pickup',
+                'Roshan Kills', 'Lane Role', 'Role', 'Fantasy Points', 'Impact'])
+    return player_frame
     
 def make_db(raw_file, OUT, type = 'Ama', pos = 'NA'):
     '''populate database file with legible match-stats'''
     
     # To convert hero_id into hero name
-    heroStats = 'heroStats.json'
+    heroStats = os.path.join('Data', 'heroStats.json')
     if not os.path.isfile(heroStats):
-        get_herostats()
+        get_herostats(heroStats)
     with open(heroStats, "r") as hs:
         hero_dict = json.load(hs)
         hero_data = pd.DataFrame.from_dict(hero_dict)    
 
     #Prepare DataFrame
-    rd2l_data = pd.DataFrame([],columns = ["Full Match", "Start Time", "Account_id", "Player",
-                                            "Fantasy Points", "Kills", "Deaths", "Assists",
-                                            "Hero Damage", "Hero Healing", "Last Hits",
-                                            "Denies", "GPM", "XPM", "Tow", "Tower Damage",
-                                            "Ros", "TF", "Obs Placed", "Camps Stacked", "Ru",
-                                            "FB", "Stuns", "Hero", "Game Length", "Total Kills",
-                                            "FPPM", "Lane", "Hero Pic", "LH@10", "Level", "Sent Placed",
-                                            "Obs Killed", "Sent Killed", "Lane Eff", "Lane Role", "Role"])
+    rd2l_data = pd.DataFrame()
+                                            
     with open(raw_file, "r") as raw:
         raw_data = json.load(raw)
                                                                                        
     with open(OUT, "w") as out_file:
-        for r in raw_data:
+        clash = 0
+        for game in raw_data:
+            print("Checking Game %s"%game['match_id'])
             role_score = 0
             gamers=[]
-            for i in r['players']:
-                #To identify first blood (for fantasy scoring)    
-                fb = i['firstblood_claimed']
-
-                #Calculate Teamfight participation
-                team_score = (r['dire_score'], r['radiant_score'])[i['isRadiant']]
-                tf_participation = round((i['kills'] + i['assists']) / team_score, 2)
-                    
-                #Calculate Fantasy Score    
-                fscore = calc_fscore(i['kills'], i['deaths'], i['last_hits'], i['denies'],
-                                        i['gold_per_min'], i['tower_kills'], i['roshan_kills'],
-                                        tf_participation, i['obs_placed'], i['camps_stacked'],
-                                        i['rune_pickups'], fb, round(i['stuns'], 2))
-                
-
-
-                
-                
-                #Convert hero_id to Hero Name
-                hero_id = i['hero_id']
-                index = hero_data.index[hero_data['id'] == hero_id].tolist()
-                hero = hero_data.iat[index[0], 2]
-                hero_pic = hero_data.iat[index[0], 6]
-                
-                #Populate DB
-                name = i['personaname']
+            obs_frame, sen_frame = ward_list(game)
+            for player in game['players']:
+                player_frame = prune_data(player, game, hero_data, obs_frame, sen_frame)
+                role = 0
+                #Attempt role detection
+                player_frame.to_csv('temp.csv', index = False)
+                name = player['personaname']
+                role = int(get_roles('role_model.joblib', 'temp.csv'))
                 if type == 'Pro':
-                    name = ''.join(i for i in i['name'] if ord(i)<128)
+                    if player['name'] == None:
+                        name = player['personaname']
+                    else:
+                        name = ''.join(i for i in player['name'] if ord(i)<128)
+                    if name == '':
+                        name = player['personaname']
                     role_db = pos[pos['Player'] == name]
                     if len(role_db['Position']) > 0:
                         role = role_db.iat[0, 1]
                     else:
-                        role = input("%s"%name)
+                        role = int(input("%s"%name))
                         new_row = pd.DataFrame([(name, role)], columns = ["Player", "Position"])
-                        print(new_row)
                         pos = pos.append(new_row, ignore_index = True)
-                        print(pos)
-                    
-                role = 0
-                start_date = datetime.date.fromtimestamp(r['start_time']).strftime("%Y-%m-%d")
-                player_frame = pd.DataFrame(
-                    [(r['match_id'], start_date, i['account_id'], name, fscore, i['kills'],
-                    i['deaths'], i['assists'],i['hero_damage'],i['hero_healing'],i['last_hits'],
-                    i['denies'], i['gold_per_min'], i['xp_per_min'], i['tower_kills'], 
-                    i['tower_damage'], i['roshan_kills'], tf_participation, i['obs_placed'],
-                    i['camps_stacked'], i['rune_pickups'], fb, i['stuns'], hero, r['duration'], 
-                    (r['dire_score']+r['radiant_score']), round(fscore*60/r['duration'],4),
-                    i['lane'], hero_pic, i['benchmarks']['lhten']['raw'], i['level'],
-                    i['sen_placed'], i['observer_kills'], i['sentry_kills'], i['lane_efficiency'], i['lane_role'], role)],
-                    columns = ["Full Match", "Start Time", "Account_id", "Player", 
-                    "Fantasy Points", "Kills", "Deaths", "Assists", "Hero Damage", 
-                    "Hero Healing", "Last Hits", "Denies", "GPM", "XPM", "Tow", 
-                    "Tower Damage", "Ros", "TF","Obs Placed", "Camps Stacked", "Ru",
-                    "FB", "Stuns", "Hero", "Game Length", "Total Kills", "FPPM",
-                    "Lane", "Hero Pic", "LH@10", "Level",
-                    "Sent Placed", "Obs Killed", "Sent Killed", "Lane Eff", "Lane Role", "Role"])
-                    
-                    
-                #Attempt role detection
-                player_frame.to_csv('temp.csv', index = False)
-                role = int(get_roles('role_model.joblib', 'temp.csv'))
-                if i['lane_role'] == 2 and role < 4:
-                    role = 2
+                    if role == 1 or role == 3 or role == 2:
+                        if role != player['lane_role']:
+                            role = int(get_roles('role_model.joblib', 'temp.csv'))
+                            print("Predicted %s for %s"%(role, name))
                 role_score += role
                 player_frame["Role"] = role
                 rd2l_data = rd2l_data.append(player_frame, ignore_index = True)
                 os.remove('temp.csv')
                 gamers.append([name, role])
             if (role_score != 30):
-                print("Check %s"%r['match_id'])
+                print("Check %s"%game['match_id'])
                 gamerframe = pd.DataFrame(gamers, columns = ["Name", "Role"])
                 print(gamerframe)
+                clash += 1
+        print(clash)        
         #Write DB to file        
         rd2l_data.to_csv(OUT, index = False)
         if type == 'Pro':
             pos.to_csv('pos_cor.dat', index = False)
         
-        
-        
+def ward_list(game):
+    obs_list = []
+    sen_list = []
+    for player in game['players']:
+        if len(player['obs_left_log']) > 0:
+            for obs_des in player['obs_left_log']:
+                obs_left = (obs_des['time'], obs_des['ehandle'])
+                obs_list.append(obs_left)
+
+        if len(player['sen_left_log']) > 0:
+            for sen_des in player['sen_left_log']:
+                sen_left = (sen_des['time'], sen_des['ehandle'])
+                sen_list.append(sen_left)
+                
+    obs_frame = pd.DataFrame(obs_list, columns = ['Time Destroyed', 'Ward ID'])
+    sen_frame = pd.DataFrame(sen_list, columns = ['Time Destroyed', 'Ward ID'])    
+
+    return obs_frame, sen_frame
+    
+def calc_impact(player, game):
+    return 0    
+    
 def makeimage(folder, player_names, point_list, date):
     '''generate a fantasy dream team image'''
     #Generate Fantasy Team Image
     top = 50
     p_top = top + 72
     h_top = p_top + 162
-    a = Image.open('fantasy_template.png').resize((1374, 459), resample = 1)
+    a = Image.open(os.path.join('Data', 'fantasy_template.png')).resize((1374, 459), resample = 1)
     hero_size = (150, 80)
     player_size = (150, 150)
     h1 = Image.open(os.path.join(folder, '1.png')).resize(hero_size, resample = 1)
@@ -220,9 +281,7 @@ def makeimage(folder, player_names, point_list, date):
     p4 = Image.open(os.path.join(folder, '4_player.png')).resize(player_size, resample = 1)
     p5 = Image.open(os.path.join(folder, '5_player.png')).resize(player_size, resample = 1)
     b = Image.new('RGB', (154,154), 'silver')
-    
-
-    
+       
     draw = ImageDraw.Draw(a)
     size = 26
     fnt = ImageFont.truetype('arial.ttf', size)
@@ -275,11 +334,10 @@ def makeimage(folder, player_names, point_list, date):
     return a   
 
 def get_roles(model, data):
-    cols = (4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 35, 36)
-    LR = load(model)
+    cols = (5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 ,23, 24, 25, 26, 27, 28, 30, 31, 32, 33)
+    rfc = load(model)
     rd2l_set = pd.read_csv(data, usecols=cols)
-    #rd2l_set = rd2l_set[rd2l_set['Role']!=2]
     rd2l_X = rd2l_set.values[:, :len(cols)-1]
     rd2l_Y = rd2l_set.values[:, len(cols)-1]
-    rd2l_predictions = LR.predict(rd2l_X)
+    rd2l_predictions = rfc.predict(rd2l_X)
     return rd2l_predictions    
--- rd2l_stats.py
+++ rd2l_stats.py
@@ -9,7 +9,12 @@ import pandas as pd
 import requests
 
 if __name__ == '__main__':
-
+    # Make Subdirectories
+    if not os.path.isdir('Data'):
+        os.mkdir('Data')
+    if not os.path.isdir('Output'):
+        os.mkdir('Output') 
+        
     #Check if days_back is given, set date after which to look for games
     try: 
         sys.argv[1]
@@ -22,13 +27,16 @@ if __name__ == '__main__':
 
     stratz_URL = 'http://api.stratz.com/api/v1/'
     opendota_URL = 'http://api.opendota.com/api/'
-    OUT = os.path.join(date_string,'rd2l_fantasy.out')
-    RAW = os.path.join(date_string,'rd2l_fantasy.raw')
-    PIC = os.path.join(date_string,'rd2l_fantasy.png')
-
+    
+    RAW = os.path.join('Output', date_string, 'rd2l_fantasy.raw')
+    DAT = os.path.join('Output', date_string, 'rd2l_fantasy.dat')    
+    PIC = os.path.join('Output', date_string, 'rd2l_fantasy.png')
+    
+      
     #Only make new DB if not existant yet
-    if not os.path.isdir(date_string):
-        os.mkdir(date_string)
+    folder = os.path.join('Output', date_string)
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
         
     if not os.path.isfile(RAW):
         print("Fetching List of Games on %s."%date_string)
@@ -38,40 +46,46 @@ if __name__ == '__main__':
             sys.exit()
         print("Populating Database")
         fu.make_raw(game_list, RAW)
-    fu.make_db(RAW, OUT)
+    fu.make_db(RAW, DAT)
 
     #Print stats    
     print("\nrd2l Stats:")
-    rd2l_data = pd.read_csv(OUT)
-
-    col_list = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 18, 19, 26] #Columns to print
-
+    rd2l_data = pd.read_csv(DAT)
+    rd2l_data['Vision Provided per Minute of Gametime'] = rd2l_data['Total Obs Duration']/(rd2l_data['Game Length']/60)
+    rd2l_data['Avg Obs Duration'] = rd2l_data['Total Obs Duration']/rd2l_data['Obs Placed']
+    col_list = [36] #Columns to print
     for col in col_list:
-        player = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), 3]
-        points = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), col]
-        hero = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), 23]
+        player = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), 2]
+        points = round(rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), col], 2)
+        hero = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), 3]
         game = rd2l_data.iat[rd2l_data[rd2l_data.columns[col]].idxmax(), 0]
         category = rd2l_data.columns[col]
         print("**Most %s** - %s on %s with %s (<https://www.opendota.com/matches/%s>)"%(category, player, hero, points, game))
+    obs_data = rd2l_data[rd2l_data['Obs Placed'] > 4].reset_index()
+    player = obs_data.iat[obs_data[obs_data.columns[38]].idxmax(), 3]
+    points = obs_data.iat[obs_data[obs_data.columns[38]].idxmax(), 38]
+    hero = obs_data.iat[obs_data[obs_data.columns[38]].idxmax(), 4]
+    game = obs_data.iat[obs_data[obs_data.columns[38]].idxmax(), 1]
+    print("**Highest average Obs Duration** - %s on %s with %s (<https://www.opendota.com/matches/%s>)"%(player, hero, points, game))    
 
-    top_heroes = rd2l_data['Hero'].value_counts()
-    print("**Top 3 Heroes Picked** - %s (%s), %s (%s), %s (%s)"%(top_heroes.index[0], top_heroes.iat[0], top_heroes.index[1], top_heroes.iat[1], top_heroes.index[2], top_heroes.iat[2]))
-    #Longest Game
-    game = rd2l_data.iat[rd2l_data["Game Length"].idxmax(), 0]
-    time = rd2l_data.iat[rd2l_data["Game Length"].idxmax(), 24]
-    print("**Longest Game** - %s (<https://www.opendota.com/matches/%s>)"%(datetime.timedelta(seconds=int(time)),game))
-    #Shortest Game
-    game = rd2l_data.iat[rd2l_data["Game Length"].idxmin(), 0]
-    time = rd2l_data.iat[rd2l_data["Game Length"].idxmin(), 24]
-    print("**Shortest Game** - %s (<https://www.opendota.com/matches/%s>)"%(datetime.timedelta(seconds=int(time)), game))
-    #Most Kills
-    game = rd2l_data.iat[rd2l_data["Total Kills"].idxmax(), 0]
-    kills = rd2l_data.iat[rd2l_data["Total Kills"].idxmax(), 25]
-    print("**Most Kills** - %s (<https://www.opendota.com/matches/%s>)"%(kills, game))
-    #Least Kills
-    game = rd2l_data.iat[rd2l_data["Total Kills"].idxmin(), 0]
-    kills = rd2l_data.iat[rd2l_data["Total Kills"].idxmin(), 25]
-    print("**Least Kills** - %s (<https://www.opendota.com/matches/%s>)"%(kills, game))
+    # top_heroes = rd2l_data['Hero'].value_counts()
+    # print("**Top 3 Heroes Picked** - %s (%s), %s (%s), %s (%s)"%(top_heroes.index[0], top_heroes.iat[0], top_heroes.index[1], top_heroes.iat[1], top_heroes.index[2], top_heroes.iat[2]))
+    # #Longest Game
+    # game = rd2l_data.iat[rd2l_data["Game Length"].idxmax(), 0]
+    # time = rd2l_data.iat[rd2l_data["Game Length"].idxmax(), 24]
+    # print("**Longest Game** - %s (<https://www.opendota.com/matches/%s>)"%(datetime.timedelta(seconds=int(time)),game))
+    # #Shortest Game
+    # game = rd2l_data.iat[rd2l_data["Game Length"].idxmin(), 0]
+    # time = rd2l_data.iat[rd2l_data["Game Length"].idxmin(), 24]
+    # print("**Shortest Game** - %s (<https://www.opendota.com/matches/%s>)"%(datetime.timedelta(seconds=int(time)), game))
+    # #Most Kills
+    # game = rd2l_data.iat[rd2l_data["Total Kills"].idxmax(), 0]
+    # kills = rd2l_data.iat[rd2l_data["Total Kills"].idxmax(), 25]
+    # print("**Most Kills** - %s (<https://www.opendota.com/matches/%s>)"%(kills, game))
+    # #Least Kills
+    # game = rd2l_data.iat[rd2l_data["Total Kills"].idxmin(), 0]
+    # kills = rd2l_data.iat[rd2l_data["Total Kills"].idxmin(), 25]
+    # print("**Least Kills** - %s (<https://www.opendota.com/matches/%s>)"%(kills, game))
     #Fantasy Dream Team
     print("**RD2L Fantasy Dream Team**")
     player_names=[]
@@ -80,14 +94,13 @@ if __name__ == '__main__':
     for i in (1,2,3,4,5):
         sort_data = rd2l_data[rd2l_data['Role']==i]
         sort_data = sort_data.sort_values(by = 'Fantasy Points', ascending = False)
-        player = sort_data.iat[0, 3]
-        points = sort_data.iat[0, 4]
-        hero = sort_data.iat[0, 23]
+        player = sort_data.iat[0, 2]
+        points = round(sort_data.iat[0, 34], 4)
+        hero = sort_data.iat[0, 3]
         game = sort_data.iat[0, 0]
-        hero_pic = sort_data.iat[0, 28]
+        hero_pic = sort_data.iat[0, 4]
         #Hero Pic
-        hero_pic = sort_data.iat[0, 28]
-        s_hp = os.path.join(date_string, str(i)+'.png')
+        s_hp = os.path.join('Output', date_string, str(i)+'.png')
         if not os.path.isfile(s_hp):
             url = 'https://api.opendota.com'+hero_pic
             pic = requests.get(url, stream = True)
@@ -96,8 +109,8 @@ if __name__ == '__main__':
                     pic.raw.decode_content = True
                     shutil.copyfileobj(pic.raw, f)
         #Player Pic
-        player_id = sort_data.iat[0,2]
-        s_pp = os.path.join(date_string, str(i)+'_player.png')
+        player_id = sort_data.iat[0, 1]
+        s_pp = os.path.join('Output', date_string, str(i)+'_player.png')
         if not os.path.isfile(s_pp):
             player_url = requests.get(opendota_URL+'players/'+str(player_id)).json()['profile']['avatarfull']
             pic = requests.get(player_url, stream = True)
@@ -113,5 +126,5 @@ if __name__ == '__main__':
     
 
     #Image
-    image = fu.makeimage(date.strftime("%Y-%m-%d"), player_names, point_list, date)
+    image = fu.makeimage(os.path.join('Output', date_string), player_names, point_list, date)
     image.save(PIC)
